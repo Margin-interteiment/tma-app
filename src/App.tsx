cc85fc091ea021dd0a99a25e3a36e1dcbeef415b
Margin-enterteiment <119576487+Margin-interteiment@users.noreply.github.com>
2025-06-18 17:17:45 +0300

    FIX | fix structure in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Root } from "./module/root/index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello!</header>
+      <header className="App-header">Main Page</header>
+      <Root />
     </div>
   );
 }
---
+++ src/module/root/index.tsx
@@ -0,0 +1,7 @@
+export const Root = () => {
+  return (
+    <div>
+      <p> Root Page</p>
+    </div>
+  );
+};
