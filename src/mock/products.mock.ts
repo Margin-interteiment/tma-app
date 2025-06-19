34b9a505e0b98f2568eab183e0c45cb210a0bd3c
Margin-enterteiment <119576487+Margin-interteiment@users.noreply.github.com>
2025-06-19 10:41:43 +0300

    FETURE | create mock content for product list

---
+++ src/mock/products.mock.ts
@@ -0,0 +1,64 @@
+import { ProductItem } from '../module/products/typing/interfaces';
+
+export const products:ProductItem[] =[
+  {
+    id: 1,
+    title: "Apple iPhone 15 Pro Max",
+    price: 43400,
+    imageUrl:"https://newtime.ua/upload/2023-09/apple-iphone-15-pro-max-256gb-blue-titanium-esim-mu693.webp"
+  },
+  {
+    id:2,
+    title: "Apple iPhone 14 Pro",
+    price: 28000,
+    imageUrl: "https://restartsp.com.ua/wp-content/uploads/2023/05/iphone-14-pro-max-gold.jpg"
+  }, 
+  {
+    id:3,
+    title: "Samsung  Galaxy S24 Ultra",
+    price: 32500,
+    imageUrl: "https://gadgetbd.com/wp-content/uploads/2024/01/Samsung-S24-Ultra-Titanium-Grey.jpg"
+  }, 
+  {
+    id:4,
+    title: "Samsung Galaxy Z Fold5",
+    price: 40500,
+    imageUrl: "https://cdn.new-brz.net/app/public/models/SM-F946BZKNSEK/large/w/240221110057785844.webp"
+  }, 
+  {
+    id:5,
+    title: "Huawei Mate 60 Pro+",
+    price: 57000,
+    imageUrl: "https://pixophone.com/image/cache/catalog/product-upload/HuaweiMate60Pro16512GBBlackCN/1HuaweiMate60Pro16512GBBlackCN-800x687.webp"
+  }, 
+  {
+    id:6,
+    title: "Huawei P60 Art",
+    price: 41000,
+    imageUrl: "https://content1.rozetka.com.ua/goods/images/big/414779173.jpg"
+  }, 
+  {
+    id:7,
+    title: "Sony Xperia 1 VI",
+    price: 26100,
+    imageUrl: "https://pcshop.ua/image/cache/webp/catalog/tovar/smartfon-sony-xperia-1-vi-12-256gb-black-global-1024x768.webp"
+  }, 
+  {
+    id:8,
+    title: "Google Pixel 8 Pro",
+    price: 1,
+    imageUrl: "https://www.dxomark.com/wp-content/uploads/medias/post-157488/Google-Pixel-8-Pro-2-featured-image-packshot-review-Recovered.jpg"
+  }, 
+  {
+    id:9,
+    title: "Xiaomi 14 Ultra",
+    price: 22000,
+    imageUrl: "https://www.dxomark.com/wp-content/uploads/medias/post-174582/Xiaomi-14-Ultra_Yoast-image-packshot-review.jpg"
+  }, 
+  {
+    id:10,
+    title: "Asus ROG Phone 8 Pro",
+    price: 60000,
+    imageUrl: "https://pcshop.ua/image/cache/webp/catalog/tovar/smartfon-asus-rog-phone-8-pro-24-1tb-phantom-black-global-1024x768.webp"
+  }, 
+]
