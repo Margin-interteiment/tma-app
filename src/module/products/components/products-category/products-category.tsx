5e7cd39aff9562d98c5d497e485f02adbdac4c05
Margin-enterteiment <119576487+Margin-interteiment@users.noreply.github.com>
2025-06-20 17:08:52 +0300

    FETURE | create a component ProductsCategory

---
+++ src/module/products/components/products-category/products-category.tsx
@@ -0,0 +1,32 @@
+import style from "./styles.module.css";
+import { Category } from "../../../products/typing/enums";
+
+type ProductsCategoryProps = {
+  selected: Category | null;
+  onSelect: (category: Category | null) => void;
+};
+
+export const ProductsCategory = ({
+  selected,
+  onSelect,
+}: ProductsCategoryProps) => {
+  return (
+    <ul className={style.listCategory}>
+      {Object.values(Category).map((item, id) => {
+        const isActive = selected === item || (selected === null && id === 0);
+        return (
+          <li key={id} className={style.itemCategory}>
+            <button
+              className={`${style.categoryBtn} ${
+                isActive ? style.categoryBtnActive : ""
+              }`}
+              onClick={() => onSelect(id === 0 ? null : item)}
+            >
+              {item}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
